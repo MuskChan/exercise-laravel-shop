c96b1b94a534ea96636a8f68007b614391cb3411
MuskChan <523919565@qq.com>
2019-10-22 17:30:43 +0800

    init

---
+++ database/factories/ProductSkuFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Models\ProductSku;
+use Faker\Generator as Faker;
+
+$factory->define(ProductSku::class, function (Faker $faker) {
+    return [
+        'title'       => $faker->word,
+        'description' => $faker->sentence,
+        'price'       => $faker->randomNumber(4),
+        'stock'       => $faker->randomNumber(5),
+    ];
+});
